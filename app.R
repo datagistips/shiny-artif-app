64c7a25268157cf832c9b57dc71b7dfffd663d9e
mrajerisonCerema <mathieu.rajerison@cerema.fr>
2021-11-05 08:54:40 +0100

    affiche un tableau des résultats

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ ui <- fluidPage(
     # Application title
     titlePanel("Artificialisation de 2009 à 2020"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
             selectInput("communes", label = NULL, choices = communes, selected = NULL),
@@ -29,14 +29,50 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-          "résultats"
+          dataTableOutput("tbResults")
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    getStatsFlux <- function(flux, codeInsee) {
+        
+        myCols <-  names(flux)[grep("^art[0-9]{2}\\S+[0-9]{2}$", names(flux))]
+        
+        # Filtre par commune
+        df <- flux %>% filter(idcom == codeInsee) 
+        
+        # Colonnes intéressantes
+        df <- df[, c("idcom", "idcomtxt", myCols)]
+        
+        # Long format
+        df <- df %>% gather("variable", # key
+                            "value",    # value
+                            myCols) # variables
+        
+        # Année et type
+        df$year <- gsub("art([0-9]{2})(\\S+)[0-9]{2}", "20\\1", df$variable)
+        df$type <- gsub("art([0-9]{2})(\\S+)[0-9]{2}", "\\2", df$variable)
+        
+        # Renomme les valeurs de type
+        df <- df %>% mutate(type = case_when(
+            type == "hab" ~ "Habitat",
+            type == "act" ~ "Activité",
+            type == "mix" ~ "Mixte",
+            type == "inc" ~ "Inconnu"))
+        
+        # Réagence les colonnes
+        df <- df[, c("idcom", "idcomtxt", "year", "type", "value")]
+        
+        return(df)
+    }
 
+    output$tbResults <- renderDataTable({
+        codeInsee <- input$communes
+        df <- flux %>% getStatsFlux(codeInsee)
+        return(df)
+    })
     
 }
 
