9c271b706e34afff002b6e6d5df7ef895af1907e
mrajerisonCerema <mathieu.rajerison@cerema.fr>
2021-11-05 08:52:05 +0100

    ajoute la liste des communes

--- app.R
+++ app.R
@@ -1,15 +1,30 @@
 library(shiny)
+library(tidyverse)
+library(sf)
+library(glue)
+
+# Flux sur PACA
+flux <- read_csv("data/obs_artif_conso_com_2009_2020_V2.csv", na = c("", "NULL")) %>% 
+    filter(idreg == "93")
+
+# Contours de communes
+comms <<- readRDS("data/comms.rds") %>% 
+    filter(INSEE_REG == 93)
+
+# Liste communes
+communes <- flux$idcom
+names(communes) <- glue("{flux$idcomtxt} ({flux$idcom})")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Artificialisation de 2009 à 2020"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            "liste"
+            selectInput("communes", label = NULL, choices = communes, selected = NULL),
         ),
 
         # Show a plot of the generated distribution
